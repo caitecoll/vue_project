caee3c3e2b93d3324f30a67f4251f5c9ff7fa710
Caitlin Collins <caitecoll@gmail.com>
2016-08-24 21:18:17 -0500

    Incorporated header components into project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,18 @@
+var exampleData = {
+    message: 'Hello, friend!'
+};
+
 window.onload = function() {
+    // define
+    var HeaderComponent = Vue.extend({
+        template: '<div>This is the Header</div>'
+    })
+
+// register
+    Vue.component('header-component', HeaderComponent)
+
     new Vue({
         el: '#app',
-        data: {
-            message: 'Hello Vue.js!'
-        }
+        data: exampleData
     })
-}
+};
